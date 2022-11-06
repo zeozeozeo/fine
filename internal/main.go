cad01be718c08d1caeecacfdf93f5db2ee7fa073
zeozeozeo <zeocat9@gmail.com>
2022-11-06 14:01:37 +0500

    Add more comments to platformer tutorial

--- internal/main.go
+++ internal/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
 	"image/color"
 	"math"
 
@@ -13,24 +12,24 @@ var (
 	gopher          *fine.Entity
 	gopherFallSpeed float64
 	gopherVelocity  float64
-	gopherDirection string // "left" or "right"
+	gopherDirection string // "left", "right" or ""
 	walkStartTime   float64
 	cameraVelocity  float64
 )
 
+// Bake gopher.png into the executable
+//
 //go:embed gopher.png
 var gopherSpriteData []byte
 
 func main() {
-	app := fine.NewApp("Demo", 1280, 720)
+	app := fine.NewApp("Platformer", 1280, 720)
 	app.SetUpdateFunc(update)
 	createEntities(app)
 	createLevel(app)
-	app.Camera.Position = gopher.Position
 
-	app.After(2, func(app *fine.App) {
-		fmt.Println("Hello, World after 2 seconds!")
-	})
+	// Make the camera start at the gopher's position
+	app.Camera.Position = gopher.Position
 
 	// Start app
 	if err := app.Run(); err != nil {
@@ -38,10 +37,12 @@ func main() {
 	}
 }
 
+// This function will be called every frame, and is used to update the game logic.
 func update(dt float64, app *fine.App) {
+	// Fall
 	gopher.Position.Y += dt * gopherFallSpeed
 
-	// Collision
+	// Collision (TODO: Find a cleaner way to do this)
 	collision := gopher.Collide()
 	if collision.TopPushY != 0 || collision.BottomPushY != 0 || collision.ForwardPushX != 0 || collision.BackwardPushX != 0 {
 		// The entity is colliding, find the smallest collisions
@@ -61,7 +62,7 @@ func update(dt float64, app *fine.App) {
 				gopher.Position.Y += collision.TopPushY
 			} else {
 				gopher.Position.Y += collision.BottomPushY
-				gopherFallSpeed = 1
+				gopherFallSpeed = 1 // Stop jumping
 			}
 		}
 	}
@@ -79,15 +80,13 @@ func update(dt float64, app *fine.App) {
 		gopherDirection = "right"
 		isWalking = true
 	} else if gopherVelocity > 0 {
+		// Stopped walking, start decreasing velocity
 		gopherVelocity -= dt * 5000
-	} else if gopherVelocity < 0 {
+	} else if gopherVelocity < 0 { // Make sure that the velocity is >= 0
 		gopherVelocity = 0
 	}
 
-	if app.IsKeyJustDown(fine.KEY_w) {
-		walkStartTime = app.Time
-	}
-
+	// Check walking direction
 	switch gopherDirection {
 	case "left":
 		gopher.Position.X -= dt * gopherVelocity
@@ -97,6 +96,14 @@ func update(dt float64, app *fine.App) {
 		gopher.FlipHorizontal()
 	}
 
+	// The walking animation could start abruptly, so subtract the time when we started
+	// walking from it.
+	if app.IsKeyJustDown(fine.KEY_w) {
+		walkStartTime = app.Time
+	}
+
+	// Walking animation. This scales a sine wave to be in the range of 0..1
+	// and adds 5 to it (because the intial scale of the gopher is 5)
 	if isWalking {
 		gopher.Scale.Y = ((math.Sin((12*(app.Time-walkStartTime)+3)*math.Pi/2) + 1) / 2) + 5
 	} else if gopher.Scale.Y > 5 {
@@ -104,27 +111,32 @@ func update(dt float64, app *fine.App) {
 	} else if gopher.Scale.Y < 5 {
 		gopher.Scale.Y = 5
 	}
+
+	// Idle animation, this is again just a scaled sine wave, but a little bit slower
 	if !isWalking {
 		gopher.Scale.Y = ((math.Sin((2*(app.Time)+3)*math.Pi/2) + 1) / 8) + 5
 	}
 
-	// Falling
+	// Fall speed
 	if collision.IsFalling {
 		gopherFallSpeed += dt * 5000
 	}
 
+	// Make sure that the fall speed is <= 1500
 	if gopherFallSpeed > 1500 {
 		gopherFallSpeed = 1500
 	}
 
 	// Jumping
 	if app.IsKeyJustDown(fine.KEY_SPACE) || app.IsKeyJustDown(fine.KEY_w) {
-		gopherFallSpeed = -1000
+		gopherFallSpeed = -1000 // This just makes the fall velocity negative, which makes us go up.
+		// Decrease X scale to animate the gopher
 		if gopher.Scale.X > 2 {
 			gopher.Scale.X -= 1
 		}
 	}
 
+	// Jumping animation
 	if gopher.Scale.X < 5 {
 		gopher.Scale.X += dt * 5
 	} else if gopher.Scale.X > 5 {
@@ -137,39 +149,53 @@ func update(dt float64, app *fine.App) {
 	camDistanceY := app.Camera.Position.Y + halfHeight - gopher.Position.Y + gopher.Height
 	isCameraMoving := false
 	isMovingOnY := false
+	isMovingDown := false
 
+	// Move camera to the right
 	if camDistanceX <= halfWidth-200 {
 		app.Camera.Position.X += dt * cameraVelocity
 		isCameraMoving = true
 	}
 
+	// Move camera to the left
 	if camDistanceX >= halfWidth*2-200 {
 		app.Camera.Position.X -= dt * cameraVelocity
 		isCameraMoving = true
 	}
 
+	// Move the camera down
 	if camDistanceY <= 300 {
 		app.Camera.Position.Y += dt * cameraVelocity
 		isCameraMoving = true
 		isMovingOnY = true
+		isMovingDown = true
 	}
 
+	// Move the camera up
 	if camDistanceY >= halfHeight*2 {
 		app.Camera.Position.Y -= dt * cameraVelocity
 		isCameraMoving = true
 		isMovingOnY = true
 	}
 
+	// Increase camera velocity if it is moving
 	if !isCameraMoving {
 		cameraVelocity = 0
 	} else {
 		cameraVelocity += dt * 2000
 	}
 
+	// Make sure that the camera velocity is smaller than the walking velocity
+	// if the camera is moving to the left or right, and smaller than the fall
+	// velocity if the camera is moving to the top or bottom
 	if !isMovingOnY && cameraVelocity > gopherVelocity {
 		cameraVelocity = gopherVelocity
 	} else if isMovingOnY && cameraVelocity > gopherFallSpeed {
-		cameraVelocity = -gopherFallSpeed
+		if isMovingDown {
+			cameraVelocity = math.Abs(gopherFallSpeed)
+		} else {
+			cameraVelocity = -gopherFallSpeed
+		}
 	}
 }
 
@@ -184,6 +210,7 @@ func createEntities(app *fine.App) {
 		SetScale(fine.NewVec2(5, 5)).
 		SetPivotCentered(true)
 
+	// This will be the background color
 	app.ClearColor = color.RGBA{54, 54, 54, 255}
 }
 
@@ -192,5 +219,5 @@ func createLevel(app *fine.App) {
 	app.Rect(fine.NewVec2(0, 700), 2000, 100, color.RGBA{255, 255, 255, 255}, true)
 
 	// Box
-	app.Rect(fine.NewVec2(100, 600), 50, 50, color.RGBA{128, 128, 128, 255}, true)
+	app.Rect(fine.NewVec2(100, 600), 50, 50, color.RGBA{0, 255, 0, 255}, true)
 }
